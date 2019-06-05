4a542dc7496afee3aa0479808b1a03f2782660f0
Elyse <ecezoonekynd@gmail.com>
2019-06-05 11:12:13 +0200

    V1

--- opdracht2/README.md
+++ opdracht2/README.md
@@ -1,3 +1,12 @@
-# frontend voor designers - opdracht 2
+Frond-end voor Designers
+Elyse Zoonekynd 500775259
+29-05-2019
 
-Tekst voor opdracht 2.
+
+2 weken geleden kreeg ik de opdracht om een slidshow te maken met vakantie foto's. Het nieuwe onderdeel was hierbij dat je ook door de foto's heen kan gaan met het keyboard.
+
+Ik vond dit een hele moeilijke opdracht omdat ik nog nooit een slideshow gemaakt heb. Op Google heb ik gelukkig veel codes kunnen vinden die ik ook begreep. Die heb ik gebruikt als bron om zo daarop verder te kunnen bouwen.
+
+Ik heb extra foto's en dots toegevoegd en de code doorgenomen.
+
+Ik heb helaas van deze varianten geen demo's omdat ik hier te weinig rekening mee gehouden heb.
--- opdracht3/v1/script.js
+++ opdracht3/v1/script.js
@@ -1,8 +1,11 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+
 var uri = "https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/movies.json";
 
+var header = document.querySelector('header');
 var section = document.querySelector('section');
-console.log("section",section);
-
 
 console.log("loadimagesmetXHR");
 var request = new XMLHttpRequest();
@@ -13,7 +16,29 @@ request.send();
 request.addEventListener("load", function(){
   var data = request.response;
   console.log("request is geladen: ",request.response);
+populateHeader(data[0]);
+populateHeader(data[1]);
+populateHeader(data[2]);
+populateHeader(data[3]);
+populateHeader(data[4]);
+populateHeader(data[5]);
   //er is data
   //nu kun je iets doen
-  section.textContent = JSON.stringify(data);
+  //section.textContent = JSON.stringify(data);});
 });
+
+function populateHeader(jsonObj) {
+  var myH1 = document.createElement('h1');
+  myH1.textContent = jsonObj['title'];
+  section.appendChild(myH1);
+
+var MyPara = document.createElement('p');
+    MyPara.textContent = jsonObj['plot']
+    section.appendChild(MyPara);
+
+var myImg = document.createElement('img')
+myImg.src = jsonObj['cover'];
+    section.appendChild(myImg);
+}
+
+
